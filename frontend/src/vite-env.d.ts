900ceecce3c72c1304fed792084cf256732c88b5
Alan Delimon <adelimon@gmail.com>
2026-02-16 13:52:38 -0500

    #525 - Migrate app builds to vite and remove create-react-app to modernize the stack

---
+++ frontend/src/vite-env.d.ts
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_API_URL: string;
+    readonly VITE_AUTH_URL: string;
+    readonly VITE_CLIENT_ID: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
