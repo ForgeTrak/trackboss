bdb64b4a4a3d7cb30c618cad1415d71f75735ce9
Alan Delimon <adelimon@gmail.com>
2026-02-19 20:01:24 -0500

    #518 - inject tenant IDs on login. Add forgetrak cert for eventaul domain changeover (#534)

--- devops/cognito/addtenantid.sh
+++ devops/cognito/addtenantid.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
+
 POOL_ID=
 TENANT_ID=
 
+TENANT_JSON="[\\\"$TENANT_ID\\\"]"
+
 for username in $(aws cognito-idp list-users \
-  --user-pool-id $POOL_ID \
+  --user-pool-id "$POOL_ID" \
   --query "Users[].Username" \
   --output text); do
 
   echo "Updating $username"
 
-  aws cognito-idp admin-update-user-attributes \
-    --user-pool-id $POOL_ID \
-    --username "$username" \
-    --user-attributes Name="custom:tenant_id",Value="$TENANT_ID"
+aws cognito-idp admin-update-user-attributes \
+  --user-pool-id "$POOL_ID" \
+  --username "$username" \
+  --user-attributes "[{\"Name\":\"custom:tenant_ids\",\"Value\":\"$TENANT_JSON\"}]"
+
 done
