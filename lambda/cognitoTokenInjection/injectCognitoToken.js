f3532d04a1535a0b2bbfca4229bc2f3b25ba2e1c
Alan Delimon <adelimon@gmail.com>
2026-02-19 20:19:16 -0500

    #518 - inject tenant IDs on login. Add forgetrak cert for eventaul domain changeover

--- lambda/cognitoTokenInjection/injectCognitoToken.js
+++ lambda/cognitoTokenInjection/injectCognitoToken.js
@@ -1,4 +1,4 @@
-export const handler = async (event) => {
+exports.handler = async (event) => {
   const rawTenantIds = event.request.userAttributes["custom:tenant_ids"];
 
   if (!rawTenantIds) return event;
