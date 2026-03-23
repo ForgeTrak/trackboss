da62eb7b341a0f2e458bbc625f38e626cafe0f09
Alan Delimon <adelimon@gmail.com>
2026-03-22 21:23:30 -0400

    #541 - create an audit log viewer - start of this feature (still need front end)

--- frontend/src/components/tabpanels/AuditLog.tsx
+++ frontend/src/components/tabpanels/AuditLog.tsx
@@ -1,12 +1,14 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Box, Text } from '@chakra-ui/react';
+import DataTable from 'react-data-table-component';
 import { UserContext } from '../../contexts/UserContext';
 import { apiRequest } from '../../controller/utils';
+import dataTableStyles from '../shared/DataTableStyles';
 
 function AuditLog() {
     const { state } = useContext(UserContext);
 
-    const [auditLogData, setAuditLogData] = useState<any>();
+    const [auditLogData, setAuditLogData] = useState<any[]>([]);
 
     async function getAuditLogData() {
         let allAuditLogs : any[] = [];
@@ -22,45 +24,69 @@ function AuditLog() {
         getAuditLogData();
     }, []);
 
+    const columns: any = [
+        {
+            name: 'Timestamp',
+            selector: (row: any) => row.created_at,
+            sortable: true,
+            id: 'timestamp',
+            maxWidth: '15%',
+        },
+        {
+            name: 'User',
+            selector: (row: any) => `${row.last_name}, ${row.first_name}`,
+            sortable: true,
+            maxWidth: '15%',
+        },
+        {
+            name: 'Action',
+            selector: (row: any) => row.user_action,
+            sortable: true,
+            maxWidth: '10%',
+        },
+        {
+            name: 'Entity Type',
+            selector: (row: any) => row.entity_type,
+            sortable: true,
+            maxWidth: '15%',
+        },
+        {
+            name: 'Entity ID',
+            selector: (row: any) => row.entity_id,
+            sortable: true,
+            maxWidth: '10%',
+        },
+        {
+            name: 'Change Details',
+            selector: (row: any) => (row.change_details ? JSON.stringify(row.change_details) : 'N/A'),
+            sortable: false,
+            wrap: true,
+        },
+    ];
+
     return (
         <Box>
             <Text fontSize="2xl" mb={4}>Audit Log</Text>
             <Text fontSize="sm" mb={4}>Audit log is a new feature, and this interface will change</Text>
             <Text fontSize="xs">Audit log only shows data for events after March, 2025.</Text>
-            {
-                auditLogData ? (
-                    auditLogData.map((log: any) => (
-                        <Box key={log.id} borderWidth="1px" borderRadius="lg" p={4} mb={4}>
-                            <Text>
-                                <strong>Action:</strong>
-                                {log.user_action}
-                            </Text>
-                            <Text>
-                                <strong>Entity Type:</strong>
-                                {log.entity_type}
-                            </Text>
-                            <Text>
-                                <strong>Entity ID:</strong>
-                                {log.entity_id}
-                            </Text>
-                            <Text>
-                                <strong>User ID:</strong>
-                                {`${log.last_name}, ${log.first_name}`}
-                            </Text>
-                            <Text>
-                                <strong>Timestamp:</strong>
-                                {log.created_at}
-                            </Text>
-                            <Text>
-                                <strong>Change:</strong>
-                                {log.change_details ? JSON.stringify(log.change_details) : 'N/A'}
-                            </Text>
-                        </Box>
-                    ))
-                ) : (
-                    <Text>No audit log data available.</Text>
-                )
-            }
+            <Box mt={4}>
+                <DataTable
+                    columns={columns}
+                    data={auditLogData}
+                    customStyles={dataTableStyles()}
+                    fixedHeaderScrollHeight="500px"
+                    highlightOnHover
+                    pagination
+                    paginationPerPage={50}
+                    paginationRowsPerPageOptions={[50, (auditLogData?.length || 999)]}
+                    responsive
+                    striped
+                    subHeaderWrap
+                    defaultSortFieldId="timestamp"
+                    defaultSortAsc={false}
+                    noDataComponent={<Text mt={4}>No audit log data available.</Text>}
+                />
+            </Box>
         </Box>
     );
 }
