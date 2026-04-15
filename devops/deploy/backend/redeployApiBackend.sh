45806acabee3eefef6748eb36bc61cbdac9ef9b3
Alan Delimon <adelimon@gmail.com>
2026-04-14 22:49:51 -0400

    #524 - Move front end to Forgetrak endpoint and update Docker deploy

--- devops/deploy/backend/redeployApiBackend.sh
+++ devops/deploy/backend/redeployApiBackend.sh
@@ -10,3 +10,7 @@ fi;
 
 apprunner_arn=`aws apprunner list-services | jq -c '.ServiceSummaryList[] | select( .ServiceName | contains("trackbossapirunner"))' | jq -r .ServiceArn`
 aws apprunner start-deployment --service-arn $apprunner_arn
+
+aws lambda update-function-code \
+    --function-name trackboss-backendhost-sta-forgeTrakApiLambdaAF7B02-jnarLYprPQq4 \
+    --image-uri 425610073499.dkr.ecr.us-east-1.amazonaws.com/pra/trackbossapi:latest
