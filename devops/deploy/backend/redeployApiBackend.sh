b315592a54c5acda18b90231129d28609e1f160d
Alan Delimon <adelimon@gmail.com>
2026-04-25 19:51:34 -0400

    Hotfix - email communication from addressing missing fix

--- devops/deploy/backend/redeployApiBackend.sh
+++ devops/deploy/backend/redeployApiBackend.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 aws lambda update-function-code \
-    --function-name trackboss-backendhost-sta-forgeTrakApiLambdaAF7B02-jnarLYprPQq4 \
+    --function-name forgetrak-prod-api-backend \
     --image-uri 425610073499.dkr.ecr.us-east-1.amazonaws.com/forgetrak/forgetrakapi:latest
