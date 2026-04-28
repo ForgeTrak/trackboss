8a7c0e4d657b355a31eb372eb431c30d5eb38714
Alan Delimon <adelimon@gmail.com>
2026-04-27 22:49:47 -0400

    Fix calendar page event list loading so it only loads once not twice.

--- frontend/src/pages/CalendarPage.tsx
+++ frontend/src/pages/CalendarPage.tsx
@@ -1,31 +1,15 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React from 'react';
 import { Box } from '@chakra-ui/react';
-import { UserContext } from '../contexts/UserContext';
 import EventCalendar from '../components/EventCalendar';
 import Header from '../components/Header';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import { getCalendarEventsAndJobs } from '../controller/event';
 
 function CalendarPage() {
-    const { state } = useContext(UserContext);
-    // 'any' is needed here for the calendar to render properly
-    const [upcomingEvents, setEvents] = useState<any[]>();
-
-    useEffect(() => {
-        async function getData() {
-            setEvents(await getCalendarEventsAndJobs(state.token));
-        }
-        getData();
-    }, []);
     return (
         <>
             <Header title="Calendar" activeButtonId={2} />
             <Box p={5} pt={3} pl={10} pr={10}>
-                {
-                    upcomingEvents && (
-                        <EventCalendar />
-                    )
-                }
+                <EventCalendar />
             </Box>
         </>
     );
