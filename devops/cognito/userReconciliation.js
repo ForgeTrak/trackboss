cbb2d4d0b4dce732fddeb112eee6664dc667da4f
Alan Delimon <adelimon@gmail.com>
2026-05-03 14:40:54 -0400

    #577 - clean up eslint issues

--- devops/cognito/userReconciliation.js
+++ devops/cognito/userReconciliation.js
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const AWS = require('aws-sdk');
+const AWS = {}; // require('aws-sdk');
 const mysql = require('mysql2');
 
 // Configure AWS SDK
