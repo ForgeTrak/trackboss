71cb90bfbb1e5d54dd945f6454a6f86b696dbe24
Alan Delimon <adelimon@gmail.com>
2026-05-04 22:23:07 -0400

    Performance - lazy load  CustomTabPanel to avoid lambda steampeeding

--- frontend/src/components/shared/CustomTabPanel.tsx
+++ frontend/src/components/shared/CustomTabPanel.tsx
@@ -17,7 +17,7 @@ interface CustomTabsProps extends Omit<TabsProps, 'children'> {
 export default function MemberSelector(props: CustomTabsProps) {
     const { tabs, panels } = props;
     return (
-        <Tabs variant="soft-rounded" bg="white" colorScheme="orange">
+        <Tabs variant="soft-rounded" bg="white" colorScheme="orange" isLazy lazyBehavior="keepMounted">
             <TabList>
                 {
                     tabs.map((tab) => (
