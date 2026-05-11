db3a61d22ebd8f578b3740365ecd5d979aca4661
Alan Delimon <adelimon@gmail.com>
2026-05-10 21:37:50 -0400

    May maint- front end stack patch

--- devops/deploy/frontend/lib/deploy-stack.ts
+++ devops/deploy/frontend/lib/deploy-stack.ts
@@ -104,12 +104,6 @@ export class DeployStack extends Stack {
             });
         }
 
-
-      const cloudfrontOutput = new CfnOutput(this, `cfdist-${domain}`, {
-        value: distribution.domainName,
-        description: 'Distribution domain name',
-        exportName: `distributionDomainName-${process.env.TRACKBOSS_ENVIRONMENT_NAME}`,
-      });
     }
   }
 }
