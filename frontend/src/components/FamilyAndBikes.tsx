31e8ffd2a6432219e17de85b5c81c59a023e907d
Alan Delimon <adelimon@gmail.com>
2026-05-28 21:58:08 -0400

    #460 - Update Bike modal so there is only one and it looks like the others

--- frontend/src/components/FamilyAndBikes.tsx
+++ frontend/src/components/FamilyAndBikes.tsx
@@ -6,9 +6,8 @@ import { useAppDisclosure } from '../hooks/useAppDisclosure';
 import { GetMemberListResponse, Member } from '../../../src/typedefs/member';
 import { Bike, GetBikeListResponse } from '../../../src/typedefs/bike';
 import DeleteAlert from './DeleteAlert';
-import EditBikesModal from './EditBikeModal';
 import AddFamilyModal from './modals/AddFamilyModal';
-import AddBikeModal from './AddBikeModal';
+import BikeModal from './modals/BikeModal';
 import { getFamilyMembers, updateMember } from '../controller/member';
 import { UserContext } from '../contexts/UserContext';
 import { createBike, deleteBike, getBikeList, updateBike } from '../controller/bike';
@@ -260,12 +259,16 @@ export default function GeneralInfo(props: cardProps) {
             }
             {
                 isEditBikeOpen && bikeToEdit && (
-                    <EditBikesModal
+                    <BikeModal
                         isOpen={isEditBikeOpen}
                         onClose={onEditBikeClose}
                         bikeToEdit={bikeToEdit}
                         // eslint-disable-next-line react/jsx-no-bind
-                        editBike={editBike}
+                        onSave={
+                            (year: string, make: string, model: string) => {
+                                editBike(bikeToEdit, year, make, model);
+                            }
+                        }
                     />
                 )
             }
@@ -283,11 +286,11 @@ export default function GeneralInfo(props: cardProps) {
             }
             {
                 isAddBikeOpen && (
-                    <AddBikeModal
+                    <BikeModal
                         isOpen={isAddBikeOpen}
                         onClose={onAddBikeClose}
                         // eslint-disable-next-line react/jsx-no-bind
-                        addBike={addBike}
+                        onSave={addBike}
                     />
                 )
             }
